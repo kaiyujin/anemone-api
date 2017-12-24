25871537044d51151fb54cea472515fdc00c092c
daisuke shiraishi <kaiyujin@gmail.com>
2017-12-24 23:36:06 +0900

    add column comment

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -1,32 +1,33 @@
--- ユーザテーブル
 CREATE TABLE users
 (
-    id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    password text NOT NULL,
-    default_shop_id BIGINT UNSIGNED NOT NULL,
-    created_by BIGINT UNSIGNED NOT NULL,
-    created_in TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    updated_by BIGINT UNSIGNED NOT NULL,
-    updated_in TEXT NOT NULL,
-    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
-
--- 権限マスタ
+    id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'ユーザID',
+    name VARCHAR(20) NOT NULL COMMENT '氏名',
+    email VARCHAR(200) NOT NULL COMMENT 'メールアドレス',
+    password text NOT NULL COMMENT 'パスワードハッシュ値',
+    default_shop_id BIGINT UNSIGNED NOT NULL COMMENT 'デフォルトショップID',
+    created_by BIGINT UNSIGNED NOT NULL COMMENT '作成者ID',
+    created_in TEXT NOT NULL COMMENT '作成モジュール名',
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '作成日時',
+    updated_by BIGINT UNSIGNED NOT NULL COMMENT '更新者ID',
+    updated_in TEXT NOT NULL COMMENT '更新モジュール名',
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '更新日時'
+)
+comment='ユーザマスタ'
+;
 CREATE TABLE authorities
 (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    authority_bit BIT(20) NOT NULL default 524288,
-    created_by BIGINT UNSIGNED NOT NULL,
-    created_in TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    updated_by BIGINT UNSIGNED NOT NULL,
-    updated_in TEXT NOT NULL,
-    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT '権限ID',
+    name VARCHAR(30) NOT NULL COMMENT '権限名',
+    authority_bit BIT(20) NOT NULL default 524288 COMMENT '権限ビット',
+    created_by BIGINT UNSIGNED NOT NULL COMMENT '作成者ID',
+    created_in TEXT NOT NULL COMMENT '作成モジュール名',
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '作成日時',
+    updated_by BIGINT UNSIGNED NOT NULL COMMENT '更新者ID',
+    updated_in TEXT NOT NULL COMMENT '更新モジュール名',
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '更新日時'
+)
+comment='権限マスタ'
+;
 /*
 options
  1桁目: 予約登録,  2桁目: 予約参照,  3桁目: 予約変更,  4桁目: 予約削除
@@ -34,16 +35,17 @@ options
  9桁目: 店舗登録, 10桁目: 店舗参照 ,11桁目: 店舗変更, 12桁目: 店舗削除
 13桁目: レポート
 */
--- ユーザ/権限交差テーブル
 CREATE TABLE users_authorities
 (
-    user_id BIGINT UNSIGNED NOT NULL,
-    authority_id INT UNSIGNED NOT NULL,
-    shop_id BIGINT UNSIGNED NOT NULL,
-    created_by BIGINT UNSIGNED NOT NULL,
-    created_in TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
-    updated_by BIGINT UNSIGNED NOT NULL,
-    updated_in TEXT NOT NULL,
-    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
-);
+    user_id BIGINT UNSIGNED NOT NULL COMMENT 'ユーザID',
+    authority_id INT UNSIGNED NOT NULL COMMENT '権限ID',
+    shop_id BIGINT UNSIGNED NOT NULL COMMENT '店舗ID',
+    created_by BIGINT UNSIGNED NOT NULL COMMENT '作成者ID',
+    created_in TEXT NOT NULL COMMENT '作成モジュール名',
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '作成日時',
+    updated_by BIGINT UNSIGNED NOT NULL COMMENT '更新者ID',
+    updated_in TEXT NOT NULL COMMENT '更新モジュール名',
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp COMMENT '更新日時'
+)
+comment='ユーザ/権限交差テーブル'
+;
