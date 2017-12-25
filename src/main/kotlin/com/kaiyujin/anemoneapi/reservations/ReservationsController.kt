88d814d0156f46b6cc5fc9d393493af90c63b25f
Daisuke Shiraishi <kaiyujin@gmail.com>
2017-12-25 19:53:44 +0900

    i18n設定の追加

--- src/main/kotlin/com/kaiyujin/anemoneapi/reservations/ReservationsController.kt
+++ src/main/kotlin/com/kaiyujin/anemoneapi/reservations/ReservationsController.kt
@@ -1,18 +1,21 @@
 package com.kaiyujin.anemoneapi.reservations
 
+import org.springframework.context.MessageSource
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping(value = "reservations")
 class ReservationsController (
-    private val reservationsService: ReservationsService
+    private val reservationsService: ReservationsService,
+    private val messageSource: MessageSource
 ) {
     @RequestMapping(value = "/{id}", method = arrayOf(RequestMethod.GET))
-    fun getReservation(@PathVariable id: Int): Reservation {
-        return Reservation(id,"fn","ln")
+    fun getReservation(@PathVariable id: Int, locale: Locale): Reservation {
+        return Reservation(id,messageSource.getMessage("test", null,  locale),"ln")
     }
 
     @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
